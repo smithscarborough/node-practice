9f87df420f62f6a361e35535d9c9aa0681b3c095
Smith Scarborough <smithscarborough@gmail.com>
2021-03-14 18:44:15 -0500

    adds minor changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer(function (req, res) {
-    res.write('Smith!!!  What is happening?!');
+    res.write('Hello, Node!');
 
     res.end();
 })
@@ -11,6 +11,6 @@ server.listen(port, function(error) {
     if (error) {
         console.log('Uh oh...something went wrong', error)
     } else {
-        console.log('Server is listening on port ' + port + ', yo!')
+        console.log('Server is listening on port ' + port)
     }
 })
